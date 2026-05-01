81c7706e6b8793ece4baf33e5f95946584c832d9
ilovetocode <pavmohan1@gmail.com>
2026-05-01 13:03:53 +0100

    build: update ox formula to v0.1.8

--- Formula/ox.rb
+++ Formula/ox.rb
@@ -1,35 +1,18 @@
 class Ox < Formula
-  desc "tmux-first CLI for managing agent sessions"
+  desc "Tmux-first CLI for managing agent sessions"
   homepage "https://github.com/ilovetocode/ox"
+  url "https://github.com/ilovetocode/homebrew-ox/raw/v0.1.8/dist/ox-v0.1.8-source.tar.gz"
+  sha256 "cca8d908f3a1956dae3a53d67b2e1355daeef5b5ec307a1ea083de1edbf87192"
   license "Proprietary"
 
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/ilovetocode/homebrew-ox/releases/download/v0.1.6/ox-v0.1.6-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "e942f7af6d9be4b19ac9143b96bf2cfa46fd8231e556227cfcdf0c61bade3396"
-    else
-      url "https://github.com/ilovetocode/homebrew-ox/releases/download/v0.1.6/ox-v0.1.6-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "ece8b371a4208613e7bda303db1a0b86202b54d4e746a190b0442338d07cab15"
-    end
-  end
-
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/ilovetocode/homebrew-ox/releases/download/v0.1.6/ox-v0.1.6-aarch64-apple-darwin.tar.gz"
-      sha256 "aa9698142770603af08628cd6f0de7dc600d65f0fd9491f91c77e5f3e163be48"
-    else
-      url "https://github.com/ilovetocode/homebrew-ox/releases/download/v0.1.6/ox-v0.1.6-x86_64-apple-darwin.tar.gz"
-      sha256 "65640395b2f55741a37c2a42f10a1f0bc96c1664e17409047863638ebfdb3e45"
-    end
-  end
-
+  depends_on "rust" => :build
   depends_on "tmux"
 
   def install
-    bin.install "ox"
+    system "cargo", "install", *std_cargo_args(path: ".")
   end
 
   test do
-    assert_match "ox", shell_output("#{bin}/ox --version")
+    assert_match "ox 0.1.8", shell_output("#{bin}/ox --version")
   end
 end
