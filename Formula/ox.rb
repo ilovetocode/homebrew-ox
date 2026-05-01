253de754e77edc34e172c477bb521353b77a83dd
ilovetocode <pavmohan1@gmail.com>
2026-05-01 15:45:43 +0100

    Update ox formula to v0.1.9

--- Formula/ox.rb
+++ Formula/ox.rb
@@ -1,18 +1,25 @@
 class Ox < Formula
   desc "Tmux-first CLI for managing agent sessions"
   homepage "https://github.com/ilovetocode/ox"
-  url "https://github.com/ilovetocode/homebrew-ox/raw/v0.1.8/dist/ox-v0.1.8-source.tar.gz"
-  sha256 "cca8d908f3a1956dae3a53d67b2e1355daeef5b5ec307a1ea083de1edbf87192"
   license "Proprietary"
 
-  depends_on "rust" => :build
   depends_on "tmux"
 
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/ilovetocode/ox/releases/download/v0.1.9/ox-v0.1.9-aarch64-apple-darwin.tar.gz"
+      sha256 "254c50ccb9610567e9493df2c18ce102ac0358d04fadfcc9efac9602af75702b"
+    else
+      url "https://github.com/ilovetocode/ox/releases/download/v0.1.9/ox-v0.1.9-x86_64-apple-darwin.tar.gz"
+      sha256 "e77564d90e49dea8403da2692fcb539f4e45e53ae4c5c0f34c25733e246e919b"
+    end
+  end
+
   def install
-    system "cargo", "install", *std_cargo_args(path: ".")
+    bin.install "ox"
   end
 
   test do
-    assert_match "ox 0.1.8", shell_output("#{bin}/ox --version")
+    assert_match "ox 0.1.9", shell_output("#{bin}/ox --version")
   end
 end
