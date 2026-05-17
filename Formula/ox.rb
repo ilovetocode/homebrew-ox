438b9b8ab459a543b5627050c2a9c06818f036a7
ilovetocode <pavmohan1@gmail.com>
2026-05-17 16:54:29 +0100

    Update ox to v0.1.21

--- Formula/ox.rb
+++ Formula/ox.rb
@@ -7,11 +7,11 @@ class Ox < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ilovetocode/ox/releases/download/v0.1.20/ox-v0.1.20-aarch64-apple-darwin.tar.gz"
-      sha256 "708575a0658f84d6919450f58b57299829e689a2ed520292714dc0d33b63866e"
+      url "https://github.com/ilovetocode/ox/releases/download/v0.1.21/ox-v0.1.21-aarch64-apple-darwin.tar.gz"
+      sha256 "6fb825d83cae44aa841d458f8eafffb3f0b61345303b32126bf0317757d095cb"
     else
-      url "https://github.com/ilovetocode/ox/releases/download/v0.1.20/ox-v0.1.20-x86_64-apple-darwin.tar.gz"
-      sha256 "85c654671fb9046e44883757bb21919dff154def04b68152482a8aec2b33af40"
+      url "https://github.com/ilovetocode/ox/releases/download/v0.1.21/ox-v0.1.21-x86_64-apple-darwin.tar.gz"
+      sha256 "4b977d85b16cf460387359650a49d9731a54178e7ae1f969191f6c9ef19e928a"
     end
   end
 
@@ -20,6 +20,6 @@ class Ox < Formula
   end
 
   test do
-    assert_match "ox 0.1.20", shell_output("#{bin}/ox --version")
+    assert_match "ox 0.1.21", shell_output("#{bin}/ox --version")
   end
 end
