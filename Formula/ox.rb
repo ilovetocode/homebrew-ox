b62a8086d8b93ef3dcd08ea09baaa583b35ce6bc
ilovetocode <pavmohan1@gmail.com>
2026-05-19 10:13:10 +0100

    Update ox to v0.1.22

--- Formula/ox.rb
+++ Formula/ox.rb
@@ -1,17 +1,38 @@
+require "download_strategy"
+require "utils/github"
+
+class GitHubPrivateReleaseAssetDownloadStrategy < CurlDownloadStrategy
+  def initialize(url, name, version, **meta)
+    token = GitHub::API.credentials
+    raise CurlDownloadStrategyError.new(url, GitHub::API::NO_CREDENTIALS_MESSAGE) if token.blank?
+
+    meta[:headers] = [
+      *meta.fetch(:headers, []),
+      "Accept: application/octet-stream",
+      "Authorization: Bearer #{token}",
+    ]
+
+    super
+  end
+end
+
 class Ox < Formula
   desc "Tmux-first CLI for managing agent sessions"
   homepage "https://github.com/ilovetocode/ox"
+  version "0.1.22"
   license "Proprietary"
 
   depends_on "tmux"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ilovetocode/ox/releases/download/v0.1.21/ox-v0.1.21-aarch64-apple-darwin.tar.gz"
-      sha256 "6fb825d83cae44aa841d458f8eafffb3f0b61345303b32126bf0317757d095cb"
+      url "https://api.github.com/repos/ilovetocode/ox/releases/assets/424046457",
+          using: GitHubPrivateReleaseAssetDownloadStrategy
+      sha256 "005b6b3b1e42c7b2b52d0ba3a3d99ca40d60d7a3ca9f0a58b89a93ab07a25283"
     else
-      url "https://github.com/ilovetocode/ox/releases/download/v0.1.21/ox-v0.1.21-x86_64-apple-darwin.tar.gz"
-      sha256 "4b977d85b16cf460387359650a49d9731a54178e7ae1f969191f6c9ef19e928a"
+      url "https://api.github.com/repos/ilovetocode/ox/releases/assets/424046461",
+          using: GitHubPrivateReleaseAssetDownloadStrategy
+      sha256 "17c1622bb858c09399a4c50c9c838e28a71afd0cde2d53dbe3dc2cc1ba9b7717"
     end
   end
 
@@ -20,6 +41,6 @@ class Ox < Formula
   end
 
   test do
-    assert_match "ox 0.1.21", shell_output("#{bin}/ox --version")
+    assert_match "ox 0.1.22", shell_output("#{bin}/ox --version")
   end
 end
