d4830645dc3c652f3a8aca35a0e188a8ef9a3789
ilovetocode <pavmohan1@gmail.com>
2026-06-06 10:32:53 +0100

    Update ox to v0.1.49

--- Formula/ox.rb
+++ Formula/ox.rb
@@ -19,20 +19,20 @@ end
 class Ox < Formula
   desc "Tmux-first CLI for managing agent sessions"
   homepage "https://github.com/ilovetocode/ox"
-  version "0.1.47"
+  version "0.1.49"
   license :cannot_represent
 
   depends_on "tmux"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://api.github.com/repos/ilovetocode/ox/releases/assets/440055371",
+      url "https://api.github.com/repos/ilovetocode/ox/releases/assets/440091343",
           using: GitHubPrivateReleaseAssetDownloadStrategy
-      sha256 "e9d7a20d4469592e13b7df76404a6a25c3e5715a8aeae4ea1ee06e1080e6efbb"
+      sha256 "4ecc59faee8c941ed985331ea8ae25e776e384da7dc889dce1f6ae6c92669628"
     else
-      url "https://api.github.com/repos/ilovetocode/ox/releases/assets/440055370",
+      url "https://api.github.com/repos/ilovetocode/ox/releases/assets/440091345",
           using: GitHubPrivateReleaseAssetDownloadStrategy
-      sha256 "6008f17752ffc2b14c7c09669e663dfdb0de837791ec3aba1cb0da7bbc6d6b8d"
+      sha256 "10e469f65979d77bb6685b771ba13dcf2f44f0f6d202e8004f6d3ada1b96b150"
     end
   end
 
@@ -41,6 +41,6 @@ class Ox < Formula
   end
 
   test do
-    assert_match "ox 0.1.47", shell_output("#{bin}/ox --version")
+    assert_match "ox 0.1.49", shell_output("#{bin}/ox --version")
   end
 end
