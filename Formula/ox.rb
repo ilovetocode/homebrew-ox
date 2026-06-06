d7a92bda6c94d191d41a5d31f76e4df1b73aed95
ilovetocode <pavmohan1@gmail.com>
2026-06-06 13:05:34 +0100

    Update ox to v0.1.53

--- Formula/ox.rb
+++ Formula/ox.rb
@@ -19,28 +19,20 @@ end
 class Ox < Formula
   desc "Tmux-first CLI for managing agent sessions"
   homepage "https://github.com/ilovetocode/ox"
-  version "0.1.52"
+  url "https://api.github.com/repos/ilovetocode/ox/releases/assets/440166761",
+      using: GitHubPrivateReleaseAssetDownloadStrategy
+  version "0.1.53"
+  sha256 "87c7925533ee535723fb357a3b15c294fee0d36237f89503f14bc068762abc72"
   license :cannot_represent
 
+  depends_on arch: :arm64
   depends_on "tmux"
-
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://api.github.com/repos/ilovetocode/ox/releases/assets/440120697",
-          using: GitHubPrivateReleaseAssetDownloadStrategy
-      sha256 "78735780128f43026399f2cde133a68f27c04a776c3ceb2e0a66da251618dfef"
-    else
-      url "https://api.github.com/repos/ilovetocode/ox/releases/assets/440120699",
-          using: GitHubPrivateReleaseAssetDownloadStrategy
-      sha256 "c9147aa4bb1d7876c5c667581f79aa381ea95c3cfea2f3a93802ef549fad8bf9"
-    end
-  end
 
   def install
     bin.install "ox"
   end
 
   test do
-    assert_match "ox 0.1.52", shell_output("#{bin}/ox --version")
+    assert_match "ox 0.1.53", shell_output("#{bin}/ox --version")
   end
 end
