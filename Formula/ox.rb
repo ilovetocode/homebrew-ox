71844aeced8781c7288d086d360c1f6d025a35da
ilovetocode <pavmohan1@gmail.com>
2026-06-23 22:08:07 +0100

    Update ox to v0.1.73

--- Formula/ox.rb
+++ Formula/ox.rb
@@ -19,10 +19,10 @@ end
 class Ox < Formula
   desc "Tmux-first CLI for managing agent sessions"
   homepage "https://github.com/ilovetocode/ox"
-  url "https://api.github.com/repos/ilovetocode/ox/releases/assets/452275937",
+  url "https://api.github.com/repos/ilovetocode/ox/releases/assets/455971056",
       using: GitHubPrivateReleaseAssetDownloadStrategy
-  version "0.1.72"
-  sha256 "532cf59e731900008d57e0291c136c6d7e66ba699d5cb8f054db2048d8b86771"
+  version "0.1.73"
+  sha256 "c4c3b815f1035f0c5dbe92fd2d28ed08c0c6cb012a401e9a2a42391b0dfee3a7"
   license :cannot_represent
 
   depends_on arch: :arm64
@@ -33,6 +33,6 @@ class Ox < Formula
   end
 
   test do
-    assert_match "ox 0.1.72", shell_output("#{bin}/ox --version")
+    assert_match "ox 0.1.73", shell_output("#{bin}/ox --version")
   end
 end
